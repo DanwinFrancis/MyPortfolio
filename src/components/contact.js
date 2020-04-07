80e34aa5f389608b7393d0b10e100187348407c4
Danwin Francis <danwinfrncs4@gmail.com>
2020-04-07 20:56:48 +0200

    added contact page to site

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
-    render() { 
-        return (
-            <div><h1>Contact Page</h1></div>
-        )
-    }
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Danwin Ambros-Francis</h2>
+            <img
+              src="https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png"
+              alt="avatar"
+              style={{ height: "250px" }}
+            />
+            {/* Take note for insline styling, attributes that normally have a hyphen between word eg. 'padding-top' would be specified
+                            as camelCase in inline styling eg. 'paddingTop' */}
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis ...
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    +27 (72) 032 0717
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-fax" aria-hidden="true"/>
+                    +27 (72) 032 0717
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    someone@example.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    @DanwinFrancis
+                  </ListItemContent>
+                </ListItem>
+                
+              </List>
+            </div>
+
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Contact;
